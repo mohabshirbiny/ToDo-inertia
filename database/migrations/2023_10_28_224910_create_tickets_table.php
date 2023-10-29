52276172436324482b27fc4e8a100f6521ed4958
mohabshirbiny <mohabelshirbiny@gmail.com>
2023-10-29 03:10:54 +0300

    commit

--- database/migrations/2023_10_28_224910_create_tickets_table.php
+++ database/migrations/2023_10_28_224910_create_tickets_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tickets', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('admin')->default(false);
-            $table->rememberToken();
+            $table->string('title');
+            $table->integer('board_id');
+            $table->enum('status', ['working', 'done'])->default('working');
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tickets');
     }
 };
